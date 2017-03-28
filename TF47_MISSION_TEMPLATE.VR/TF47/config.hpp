f20a6bf80b02661cc94b04101948c6aeaa7d637e
mindbl4ster <saschatrunz@yahoo.de>
2017-03-28 09:57:28 +0200

    restructure of settingsystem

--- TF47_MISSION_TEMPLATE.VR/TF47/config.hpp
+++ TF47_MISSION_TEMPLATE.VR/TF47/config.hpp
@@ -1,51 +1,66 @@
-/*
-	set settings for radiocommunication if mod taskforce radio arrowhead radio is used
-	RADIOCHANNEL 0: "Radio Communication - Public I"
-	RADIOCHANNEL 1: "Radio Communication - Event"
-	RADIOCHANNEL 2: "Radio Communication - DEV"
-*/
+class tf47_settings
+{
+  class radiosettings
+  {
+    /*
+    	set settings for radiocommunication if mod taskforce radio arrowhead radio is used
+    	RADIOCHANNEL 0: "Radio Communication - Public I"
+    	RADIOCHANNEL 1: "Radio Communication - Event"
+    	RADIOCHANNEL 2: "Radio Communication - DEV"
+    */
+    value = 1;
+    type = "SCALAR";
+  };
+  class unitTracking
+  {
+    /*
+    	set option to activate tracker for specific side and unittypes on map
+    	TRACKING 1	-	will show markers on map
+    	TRACKING 0	-	will not track units
+    */
+    value = 1;
+    type = "SCALAR";
+  };
+  class unitTracking_sides
+  {
+    /*
+    	set option to display tracking marker for specific side
+    	possible options: WEST, EAST, INDEPENDENT, CIVILIAN
+    */
+    value[]=[WEST,EAST,INDEPENDENT,CIVILIAN];
+    type = "ARRAY";
+  };
+  class unitTracking_types
+  {
+    /*
+    	set option to display tracking marker for specific unittypes
+    	possible options: "man", "car", "ship", "air", "static", "tank"
+    */
+    value[] = ["car","ship","air","static","tank"];
+    type = "ARRAY";
+  };
+  class whitelist
+  {
+    /*
+      active option to track and enforce tf47 whitelst with reporting feature to tf47 gadget
+      CAUTION: extDB3 needed for extensioncalls
+      WHITELIST true - whitelist enforcing active
+      WHITELIST false - whitelist enforcing not active
 
-#define RADIOCHANNEL 1
+      if you want to use the whitelist functions make sure you have an active connection to a database
+      furthermore player characters need following initialisation:
 
-/*
-	set option to activate tracker for specific side and unittypes on map
-	TRACKING true	-	will show markers on map
-	TRACKING false	-	will not track units
-*/
+      nothing 0
+      helo 1
+      jet 2
+      tank 3
+      builder 4
+      jtfc 5
 
-#define TRACKING true
-
-/*
-	set option to display tracking marker for specific side
-	possible options: WEST, EAST, INDEPENDENT, CIVILIAN
-*/
-
-#define SIDES	[WEST,EAST,INDEPENDENT,CIVILIAN]
-
-/*
-	set option to display tracking marker for specific unittypes
-	possible options: "man", "car", "ship", "air", "static", "tank"
-*/
-
-#define TYPES	["car","ship","air","static","tank"]
-
-/*
-  active option to track and enforce tf47 whitelst with reporting feature to tf47 gadget
-  CAUTION: extDB3 needed for extensioncalls
-  WHITELIST true - whitelist enforcing active
-  WHITELIST false - whitelist enforcing not active
-
-  if you want to use the whitelist functions make sure you have an active connection to a database
-  furthermore player characters need following initialisation:
-
-  nothing 0
-  helo 1
-  jet 2
-  tank 3
-  builder 4
-  jtfc 5
-
-  this setVariable ["tf47_whitelist_specialCharacter",0];
-*/
-
-#define WHITELIST true
+      this setVariable ["tf47_whitelist_specialCharacter",0];
+    */
+    value = 1;
+    type = "SCALAR";
+  };
+};
+#include "ACE\aceSettings.hpp"
--- TF47_MISSION_TEMPLATE.VR/TF47/init.sqf
+++ TF47_MISSION_TEMPLATE.VR/TF47/init.sqf
@@ -1,13 +1,24 @@
 // do not edit unless you know what you are doing!
-#include "config.hpp"
 #include "tf47_macros.hpp"
 
-[] call tf47_misc_fnc_transfereUnits;
-[ RADIOCHANNEL ] call tf47_misc_fnc_radiosettings;
-[ TRACKING, SIDES, TYPES ] call tf47_misc_fnc_trackUnits;
-
-if WHITELIST then{
-  DTRACE_1("[ INFO ] > 'Whitelist' > Initialization ...");
-  [] call tf47_whitelist_fnc_initServer;
-  [] call tf47_whitelist_fnc_initClient;
+// check wether tf47 settings hpp is present or not
+if(isClass (missionConfigFile >> "tf47_settings") )then{
+  if(isClass (missionConfigFile >> "cfgFunctions" >> "tf47_misc") )then{
+    [] call tf47_misc_fnc_transfereUnits;
+    [
+      getNumber (missionConfigFile >> "tf47_settings" >> "radiosettings" >> "value")
+    ] call tf47_misc_fnc_radiosettings;
+    [
+      getNumber (missionConfigFile >> "tf47_settings" >> "unitTracking" >> "value"),
+      getArray (missionConfigFile >> "tf47_settings" >> "unitTracking_sides" >> "value"),
+      getArray (missionConfigFile >> "tf47_settings" >> "unitTracking_types" >> "value")
+    ] call tf47_misc_fnc_trackUnits;
+  };
+  if(isClass (missionConfigFile >> "cfgFunctions" >> "tf47_whitelist") )then{
+    if(getNumber (missionConfigFile >> "tf47_settings" >> "whitelist" >> "value") > 0)then{
+      DTRACE_1("[ INFO ] > 'Whitelist' > Initialization ...");
+      [] call tf47_whitelist_fnc_initServer;
+      [] call tf47_whitelist_fnc_initClient;
+    };
+  };
 };
